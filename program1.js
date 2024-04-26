7e82097c3f530be474d1db8a870de56fbc67db1d
Rupalineelkanth <128832354+Rupalineelkanth@users.noreply.github.com>
2024-04-26 13:32:08 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,16 @@
 function smallestMissingPositiveInteger(nums) {
 
 
-    // Implement the function smallest_missing_positive_integer
-  
+  // Implement the function smallest_missing_positive_integer
+  nums.sort();
+  let number = 1;
+  let i = 0;
+  while(i<nums.length && nums[i]<=0) i++;
+  for(i; i<nums.length; i++) {
+    if(nums[i] != number) return number;
+    number++;
   }
-  
-  module.exports = smallestMissingPositiveInteger;
+  return number;
+}
 
+module.exports = smallestMissingPositiveInteger;
